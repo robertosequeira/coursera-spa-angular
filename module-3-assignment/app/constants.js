33297b2f63065a29a76b94019d5fe0acc8b60a18
Roberto Sequeira <roberto@roberto-sequeira.com>
2016-10-03 22:06:00 -0600

    Use protoco-relative base url

--- module-3-assignment/app/constants.js
+++ module-3-assignment/app/constants.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular.module('NarrowItDownApp')
-    .constant('ApiBasePath', "http://davids-restaurant.herokuapp.com");
-    
+    .constant('ApiBasePath', "//davids-restaurant.herokuapp.com");
+
 })();
